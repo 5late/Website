e3118e5207597c6d06b4f431ab715a94f6381e6e
5late <markandmichfamily@gmail.com>
2020-08-01 16:53:19 -0400

    started preloader effect

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {scrollDown()};
+var overlay - 
+document.getElementById("preLoader")
 
-// Get the header
-var Entry = document.getElementsById("Entry");
-
-// Get the offset position of the navbar
-var sticky = Entry.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function scrollDown() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
+window.addEventListener('load', function()
+{
+  preLoader.style.display = 'none';
+})
